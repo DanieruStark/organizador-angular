731b25a4991e861664bef9266bbd792f108a7f2d
Daniel Santana <danielmaster19@gmail.com>
2023-01-06 06:31:24 -0300

    deixando a tabela mais estilizada

--- src/app/despesas/despesas.module.ts
+++ src/app/despesas/despesas.module.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { DespesasRoutingModule } from './despesas-routing.module';
 import { DespesasComponent } from './despesas/despesas.component';
 
 @NgModule({
   declarations: [DespesasComponent],
-  imports: [CommonModule, DespesasRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    DespesasRoutingModule,
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule,
+  ],
 })
 export class DespesasModule {}
